253effeeeecf7401dbb11fa53a606e9ded9f8d31
Sam Watson <sam@toomuch.com.au>
2014-05-14 20:33:04 +0800

    Initial commit

---
+++ Erudite Trading/ETAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ETAppDelegate.h
+//  Erudite Trading
+//
+//  Created by Sam Watson on 14/05/14.
+//  Copyright (c) 2014 Sam Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Erudite Trading/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Erudite Trading
+//
+//  Created by Sam Watson on 14/05/14.
+//  Copyright (c) 2014 Sam Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ETAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ETAppDelegate class]));
+    }
+}
