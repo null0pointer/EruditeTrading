033a952ff9e497daf6298f703752cf9e0fd2ea55
Sam Watson <sam@toomuch.com.au>
2014-05-21 20:23:09 +0800

    Added labels for price, ask, bid on front page

--- Erudite Trading/ETRootViewController.m
+++ Erudite Trading/ETRootViewController.m
@@ -12,7 +12,9 @@
 
 @interface ETRootViewController ()
 
-@property (strong, nonatomic) UITextView *textView;
+@property (strong, nonatomic) UILabel *lastPriceLabel;
+@property (strong, nonatomic) UILabel *bidLabel;
+@property (strong, nonatomic) UILabel *askLabel;
 
 @end
 
@@ -31,15 +33,53 @@
 {
     [super viewDidLoad];
     
-    self.textView = [[UITextView alloc] initWithFrame:self.view.bounds];
-    self.textView.editable = NO;
-    [self.view addSubview:self.textView];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    [[ETBitstampConnector shared] accountBalanceWithSuccess:^(id responseObject) {
-        self.textView.text = [responseObject description];
-    } failure:^(NSError *error) {
-        NSLog(@"ERROR: %@", error.description);
+    self.lastPriceLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 100.0)];
+    self.bidLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.lastPriceLabel.frame.size.height, self.view.frame.size.width / 2, 44.0)];
+    self.askLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2, self.lastPriceLabel.frame.size.height, self.view.frame.size.width / 2, 44.0)];
+    
+    self.lastPriceLabel.text = @"$0.00";
+    self.bidLabel.text = @"$0.00";
+    self.askLabel.text = @"$0.00";
+    
+    self.lastPriceLabel.textAlignment = NSTextAlignmentCenter;
+    self.lastPriceLabel.font = [UIFont fontWithName:@"AvenirNext-Regular" size:54.0];
+    self.lastPriceLabel.textColor = [UIColor blackColor];
+    
+    self.bidLabel.textAlignment = NSTextAlignmentCenter;
+    self.bidLabel.font = [UIFont fontWithName:@"AvenirNext-Regular" size:24.0];
+    self.bidLabel.textColor = [UIColor greenColor];
+    
+    self.askLabel.textAlignment = NSTextAlignmentCenter;
+    self.askLabel.font = [UIFont fontWithName:@"AvenirNext-Regular" size:24.0];
+    self.askLabel.textColor = [UIColor redColor];
+    
+    [self.view addSubview:self.lastPriceLabel];
+    [self.view addSubview:self.bidLabel];
+    [self.view addSubview:self.askLabel];
+    
+    [[ETBitstampConnector shared] subscribeToLiveTrades:^(id trade) {
+        NSNumber *price = [trade valueForKey:@"price"];
+        self.lastPriceLabel.text = [NSString stringWithFormat:@"$%@", price];
     }];
+    
+    [[ETBitstampConnector shared] subscribeToLiveOrders:^(id orders) {
+        NSArray *bids = [orders valueForKey:@"bids"];
+        NSArray *asks = [orders valueForKey:@"asks"];
+        
+        NSArray *highestBid = [bids objectAtIndex:0];
+        NSArray *lowestAsk = [asks objectAtIndex:0];
+        
+        self.bidLabel.text = [NSString stringWithFormat:@"$%@", [highestBid objectAtIndex:0]];
+        self.askLabel.text = [NSString stringWithFormat:@"$%@", [lowestAsk objectAtIndex:0]];
+    }];
+    
+//    [[ETBitstampConnector shared] accountBalanceWithSuccess:^(id responseObject) {
+//        
+//    } failure:^(NSError *error) {
+//        NSLog(@"ERROR: %@", error.description);
+//    }];
 }
 
 - (void)didReceiveMemoryWarning
